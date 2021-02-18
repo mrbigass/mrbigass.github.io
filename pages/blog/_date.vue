f214d0549a5ff93bf4dbb4059128f6a420fcf8f8
mrbigass <wetsand.wfs@gmail.com>
2021-02-18 22:45:36 +0900

    nodata

--- pages/blog/_date.vue
+++ pages/blog/_date.vue
@@ -11,9 +11,13 @@ import { Context } from 'node-sass';
 export default {
   async asyncData(context: Context): Promise<{ markdownRawText: string }> {
     const { params } = context
-    const importedModule = await require(`~/markdowns/blog/${params.date}.md`)
-    const markdownRawText: string = importedModule.default
-    return { markdownRawText }
+    try {
+      const importedModule = await require(`~/markdowns/blog/${params.date}.md`)
+      const markdownRawText: string = importedModule.default
+      return { markdownRawText }
+    } catch {
+      return { markdownRawText: 'NoData' }
+    }
   },
   data() {
     return {
