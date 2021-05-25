6c9ba5a70c2644fcd85a310a02d32f31239ee64b
mrbigass <wetsand.wfs@gmail.com>
2021-05-25 22:39:14 +0900

    パスパラメータから読み込み

---
+++ pages/blog/_date/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <markdown-loader
+    :markdownFilePath="markdownFilePath"
+  >
+  </markdown-loader>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class Blog extends Vue {
+  get markdownFilePath() {
+    return `blog/${this.$route.params.date}`;
+  }
+}
+</script>
